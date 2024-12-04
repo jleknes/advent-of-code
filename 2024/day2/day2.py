be77f9eab65495fec333b3bf85cd33009024f318
Jørund Leknes <jorund.leknes@gmail.com>
2024-12-04 13:37:26 +0100

    refactor day2

--- 2024/day2/day2.py
+++ 2024/day2/day2.py
@@ -4,14 +4,9 @@ from typing import List
 def read_input() -> List[List[int]]:
     return [[int(token) for token in line.split()] for line in fileinput.input()]
 
-def is_increasing_sequence(sequence: List[int]) -> bool:
-    return all(0 < sequence[i] - sequence[i-1] < 4 for i in range(1, len(sequence)))
-
-def is_decreasing_sequence(sequence: List[int]) -> bool:
-    return all(0 < sequence[i-1] - sequence[i] < 4 for i in range(1, len(sequence)))
-
-def is_safe(line: List[int]) -> bool:
-    return is_increasing_sequence(line) or is_decreasing_sequence(line)
+def is_safe(sequence: List[int]) -> bool:
+    return (all(0 < sequence[i] - sequence[i-1] < 4 for i in range(1, len(sequence))) or 
+            all(0 < sequence[i-1] - sequence[i] < 4 for i in range(1, len(sequence))))
 
 def solve_part_two(lines: List[List[int]]):
     safe_reports = sum(
