c469395a26a224141e0f06982fff67ee0e524277
Jørund Leknes <jorund.leknes@gmail.com>
2024-12-09 20:08:32 +0100

    refactor day9

--- 2024/day9/day9.py
+++ 2024/day9/day9.py
@@ -1,52 +1,40 @@
 import fileinput
 
-
 def read_input() -> str:
     return next(fileinput.input()).strip()
 
+def get_disk(disk_map:str):
+    return [
+        int(i / 2) if i % 2 == 0 else "."
+        for i, c in enumerate(disk_map)
+        for _ in range(int(c))
+    ]
+
+def get_checksum(disk):
+    return sum(i * val for i, val in enumerate(disk) if val != ".")
 
 def solve_part_one(disk_map: str):
-    print(disk_map)
-    disk = []
-    for i, c in enumerate(disk_map):
-        for j in range(int(c)):
-            if i % 2 == 0:
-                disk.append(int(i / 2))
-            else:
-                disk.append(".")
-    print(disk)
-    i = 0
-    j = len(disk) - 1
+    disk = get_disk(disk_map)
+
+    i, j = 0, len(disk) - 1
     while i < j:
         if disk[i] != ".":
             i += 1
         else:
-            disk[i] = disk[j]
-            del disk[j]
+            disk[i], disk[j] = disk[j], disk[i]
             j -= 1
-
-    print(disk)
-    checksum = 0
-    for i in range(len(disk)):
-        checksum += i * disk[i]
-    print(checksum)
+    print(get_checksum(disk))
 
 def solve_part_two(disk_map: str):
-    disk = []
-    highest_id=0
-    for i, c in enumerate(disk_map):
-        for j in range(int(c)):
-            if i % 2 == 0:
-                disk.append(int(i / 2))
-                highest_id=int(i/2)
-            else:
-                disk.append(".")
+    disk = get_disk(disk_map)
+    highest_id = max(val for val in disk if val != ".")
+
     for i in range(highest_id, -1, -1):
         first_pos = disk.index(i)
         last_pos = len(disk) - 1 - disk[::-1].index(i)
-        min_length = last_pos-first_pos+1
-        start_pos = -1
-        count = 0
+        min_length = last_pos - first_pos + 1
+
+        start_pos, count = -1, 0
         for k in range(first_pos):
             if disk[k] == ".":
                 if count == 0:
@@ -56,24 +44,17 @@ def solve_part_two(disk_map: str):
                     break
             else:
                 count = 0
+
         if count == min_length:
             for k in range(start_pos, start_pos + min_length):
-                disk[k] = i
-                disk[last_pos-(k-start_pos)]="."
+                disk[k], disk[last_pos - (k - start_pos)] = i, "."
 
-    checksum = 0
-    for i in range(len(disk)):
-        if disk[i] != ".":
-            checksum += i * disk[i]
-    print(checksum)
-
-
+    print(get_checksum(disk))
 
 def main() -> None:
     disk_map = read_input()
     solve_part_one(disk_map)
     solve_part_two(disk_map)
-
 
 if __name__ == "__main__":
     main()
