d8fae6bbd095868cc74059fefacd88fceabcacaa
Jørund Leknes <jorund.leknes@gmail.com>
2024-12-10 10:28:42 +0100

    refactor day10 2024

--- 2024/day10/day10.py
+++ 2024/day10/day10.py
@@ -1,16 +1,13 @@
 import fileinput
-from typing import List, Tuple
-from collections import deque as queue
+from typing import List, Set, Tuple
+from collections import deque
 
 
 def read_input() -> List[List[int]]:
-    grid = []
-    for line in fileinput.input():
-        grid.append([int(x) for x in line.strip()])
-    return grid
+    return [[int(x) for x in line.strip()] for line in fileinput.input()]
 
 
-def move(y, x, direction: str) -> Tuple[int, int]:
+def move(y: int, x: int, direction: str) -> Tuple[int, int]:
     moves = {
         "NORTH": (-1, 0),
         "EAST": (0, 1),
@@ -21,50 +18,49 @@ def move(y, x, direction: str) -> Tuple[int, int]:
     return y + dy, x + dx
 
 
-def score(grid, start_point):
-    q = queue()
-    q.append(start_point)
-
+def score(grid: List[List[int]], start_point: Tuple[int, int], part: str) -> int:
+    q = deque([start_point])
     score = 0
-    while len(q) > 0:
-        print(q)
+    targets_reached: Set[Tuple[int, int]] = set()
+
+    while q:
         y, x = q.popleft()
         if grid[y][x] == 9:
-            print(y, x, grid[y][x])
+            targets_reached.add((y, x))
             score += 1
         else:
             for direction in ["NORTH", "EAST", "SOUTH", "WEST"]:
-                pos = move(y, x, direction)
-                # print(y, x, direction, pos)
-                if 0 <= pos[0] < len(grid) and 0 <= pos[1] < len(grid[0]):
-                    height = grid[pos[0]][pos[1]]
-                    # print("height:", height)
-                    if height == grid[y][x] + 1:
-                        q.append(pos)
+                ny, nx = move(y, x, direction)
+                if 0 <= ny < len(grid) and 0 <= nx < len(grid[0]):
+                    if grid[ny][nx] == grid[y][x] + 1:
+                        q.append((ny, nx))
 
-        # print("y,x", y, x)
-    return score
-    # if y > 0 and grid[y-1][x] > grid[y][x]:
+    return len(targets_reached) if part == "ONE" else score
 
 
-def solve_part_one(grid):
+def get_start_points(grid):
     start_points = []
     for y in range(len(grid)):
         for x in range(len(grid[y])):
             if grid[y][x] == 0:
                 start_points.append((y, x))
-    print(grid)
-    print(start_points)
-    print(score(grid, start_points[0]))
-    # for start_point in start_points:
-    # print(start_point)
-    # print(score(grid, start_point))
+    return start_points
+
+
+def solve_part_one(grid):
+    start_points = get_start_points(grid)
+    print(sum(score(grid, start_point, "ONE") for start_point in start_points))
+
+
+def solve_part_two(grid):
+    start_points = get_start_points(grid)
+    print(sum(score(grid, start_point, "TWO") for start_point in start_points))
 
 
 def main() -> None:
     grid = read_input()
     solve_part_one(grid)
-    # solve_part_two(disk_map)
+    solve_part_two(grid)
 
 
 if __name__ == "__main__":
