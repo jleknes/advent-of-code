69cb5b8db38dbce1e42473018ef18ee6e646f0cc
Jørund Leknes <jorund.leknes@gmail.com>
2024-12-12 16:56:04 +0100

    WIP: day12 part two

--- 2024/day12/day12.py
+++ 2024/day12/day12.py
@@ -51,6 +51,45 @@ def find_region(grid, start):
     return region
 
 
+def border_cost(border):
+    # for each remaining border point:
+    # check if any other points are on a straight line
+    # -+ +-
+    # A| |A
+    # A+-+A
+    # AAAAA
+    # if they are on a line, add
+
+    # All squares with more than one point is a cross.
+    # change data structure?
+    remaining_points = list(border)
+    (y, x) = remaining_points[0]
+    remaining_points.remove((y,x))
+    price=1
+    #current_fence = [(y,x)]
+
+    while remaining_points:
+        # check all neighbours. If there is a neighbouring border,
+        # check all neighbours in both directions on the same line. do so until
+        # no more neighbours in those directions.
+        # of we have to turn, start a new "collection"
+        neighbours = neighbouring_points(y, x)
+        for neighbour in neighbours:
+            if neighbour in remaining_points:
+                remaining_points.remove(neighbour)
+                if neighbour[0]!=y:
+                    #look for vertical points
+                else:
+                    #look for horizontal points
+                break
+        (y, x) = remaining_points[0]
+        remaining_points.remove((y,x))
+        price+=1
+
+        # for neighbour in remaining_points:
+        #    if neighbour in
+    return price
+
 def solve_part_one(grid):
     gridsize = len(grid)
     regions = []
@@ -66,14 +105,16 @@ def solve_part_one(grid):
         border = calculate_border(region)
         price += len(border) * len(region)
         # print region in a grid
-        """for y in range(gridsize):
-            for x in range(gridsize):
+        for y in range(-1, gridsize + 1):
+            for x in range(-1, gridsize + 1):
                 if (y, x) in region:
-                    print("O", end="")
+                    print("o", end="")
+                elif (y, x) in border:
+                    print(border.count((y, x)), end="")
                 else:
                     print(".", end="")
             print()
-        print()"""
+        print()
     print(price)
 
 
