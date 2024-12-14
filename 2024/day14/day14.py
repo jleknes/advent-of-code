c002b7fd6366f58111c383d9afd140a890effdbf
Jørund Leknes <jorund.leknes@gmail.com>
2024-12-14 14:58:20 +0100

    refactor day14

--- 2024/day14/day14.py
+++ 2024/day14/day14.py
@@ -1,28 +1,28 @@
 import fileinput
 import math
-import time
 
 def read_input() -> list[str]:
     robots = []
     for line in fileinput.input():
         parts = line.strip().split()
-
-        pos = [int(x) for x in parts[0][2:].split(",")]
-        velocity = [int(x) for x in parts[1][2:].split(",")]
-        robots.append((pos, velocity))
+        robot = {}
+        robot["x"], robot["y"]= [int(x) for x in parts[0][2:].split(",")]
+        robot["vx"], robot["vy"] = [int(x) for x in parts[1][2:].split(",")]
+        robots.append(robot)
     return robots
 
 def solve_part_two(robots, gridsize):
     for second in range(10000):
-        end_states=[]
+        positions=[]
         for robot in robots:
-            x,y = (robot[0][0]+robot[1][0]*second)%gridsize[0], (robot[0][1]+robot[1][1]*second)%gridsize[1]
-            end_states.append((x,y))        
-        if len(end_states) == len(set(end_states)):
+            x = (robot["x"]+robot["vx"]*second)%gridsize[0]
+            y = (robot["y"]+robot["vy"]*second)%gridsize[1]
+            positions.append((x,y))        
+        if len(positions) == len(set(positions)):
             print (second)
             for y in range (gridsize[1]):
                 for x in range (gridsize[0]):
-                    if (x,y) in end_states:
+                    if (x,y) in positions:
                         print("1", end="")
                     else:
                         print(".", end="")
@@ -33,13 +33,13 @@ def solve_part_two(robots, gridsize):
 def solve_part_one(robots, gridsize):
     quadrants = [0] * 4
     mid_x, mid_y = (gridsize[0] - 1) / 2, (gridsize[1] - 1) / 2
+    second = 100
 
     for robot in robots:
-        x = (robot[0][0]+robot[1][0]*100)%gridsize[0]
-        y = (robot[0][1]+robot[1][1]*100)%gridsize[1]
+        x = (robot["x"]+robot["vx"]*second)%gridsize[0]
+        y = (robot["y"]+robot["vy"]*second)%gridsize[1]
         if x == mid_x or y == mid_y:
-            continue # Skip to the next robot
-        
+            continue
         quadrant_index = (int(x > mid_x) + 2 * int(y > mid_y))
         quadrants[quadrant_index] += 1        
     print(math.prod(quadrants))
