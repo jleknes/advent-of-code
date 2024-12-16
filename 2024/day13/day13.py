70a96ce8595cd68a8eca8caef51ccd966caef60e
Jørund Leknes <jorund.leknes@gmail.com>
2024-12-16 08:38:45 +0100

    WIP: day 13 part two 2024

--- 2024/day13/day13.py
+++ 2024/day13/day13.py
@@ -1,5 +1,6 @@
 import fileinput
 import re
+import math
 
 def read_input() -> list[str]:
     # I want a list of cases. each case are three lines, then follow a newline
@@ -27,7 +28,8 @@ def read_input() -> list[str]:
     
 
 def min_tokens(machine):
-    min_tokens=10000
+    print(machine)
+    min_tokens=1000000
     for a in range(0,101):
         for b in range(0,101):
             if (machine["button_a"]["x"]*a+machine["button_b"]["x"]*b==machine["prize"]["x"] and
@@ -40,17 +42,96 @@ def solve_part_one(machines):
     tokens = 0
     for machine in machines:
         tokens_spent = min_tokens(machine)
-        if tokens_spent!=10000:
+        if tokens_spent!=1000000:
             tokens+=tokens_spent
     print(tokens)
 
+"""
+Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=10000000008400, Y=10000000005400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=10000000012748, Y=10000000012176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=10000000007870, Y=10000000006450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=10000000018641, Y=10000000010279
+"""
+
+def solve_two(machine, prize):
+    # finn alle faktorer i x og y.
+    # sjekk par av faktorer. det må finnes en x-faktor og y-faktor som tilfredsstiller x/xfaktor =y/yfaktor
+    
+
+    costs = []
+    tokens = 10000000000000
+    x_factors = get_factors(machine["prize"]["x"])
+    y_factors = get_factors(machine["prize"]["y"])
+    for x_factor in x_factors:
+        for y_factor in y_factors:
+            if machine["prize"]["x"]//x_factor==machine["prize"]["y"]//y_factor:
+                print (x_factor, y_factor)
+                machine_2 = dict.copy(machine)
+                machine_2["prize"]["x"]=x_factor
+                machine_2["prize"]["y"]=y_factor
+                multiplier = machine["prize"]["x"]//x_factor
+                print (min_tokens(machine_2)*multiplier)
+                cost = min_tokens(machine_2)
+                if cost!=1000000:
+                    costs.append(min_tokens(machine_2)*multiplier)
+    if (len(costs)>1):
+        print(min(costs))
+    else:
+        print(-1)
+    #for a in range(int(max(math.sqrt(prize["x"]), math.sqrt(prize["y"])))):
+        #for b in range(int(max(math.sqrt(prize["x"]), math.sqrt(prize["y"])))):
+            #c=a*b
+            #if (a*machine["a_button"]["x"]+b*machine["b_button"]["x"] in x_factors and 
+            #a*machine["a_button"]["y"]+b*machine["b_button"]["y"] in y_factors):
+            #    costs+=
+                
+    # try to solve for all factors up until sqrt(x), sqrt(y)
+    # if solution is found, multiply with prize.x/factor
+
+def get_factors(n):
+    factors = []
+    for i in range(1, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            factors.append(i)
+            if i != n // i:
+                factors.append(n // i)
+    factors.sort()
+    return factors
+
+
+# Button A: X+94, Y+34
+# Button B: X+22, Y+67
+# Prize: X=10000000008400, Y=10000000005400
+
+# a*94+b*22=10000000008400 
+# a*34+b*67=10000000005400
+#Button A: X+26, Y+66
+#Button B: X+67, Y+21
+#Prize: X=10000000012748, Y=10000000012176
+# a*26+b*67=10000000008400 
+# a*66+b*21=10000000005400
+
+
+
+
 def solve_part_two(machines):
-    tokens = 0
-    for machine in machines:
-        #tokens_spent = min_tokens(machine)
-        #if tokens_spent!=10000:
-        #    tokens+=tokens_spent
-    print(tokens)
+    print(machines)
+    for i, machine in enumerate(machines):
+        print(f"machine nr {i+1}")
+        print("x factors", get_factors(machine["prize"]["x"] + 10000000000000))
+        print("y factors", get_factors(machine["prize"]["y"] + 10000000000000))
+        solve_two(machine, {"x":machine["prize"]["x"] + 10000000000000, "y":machine["prize"]["y"] + 10000000000000})
 
 
 def main() -> None:
